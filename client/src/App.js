585bac4975a1c1c38eb11cb7f7b97c750493426d
PriyangaPKini <priyangapkini99@gmail.com>
2020-05-15 23:53:32 +0530

    Completed the restful api part

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
