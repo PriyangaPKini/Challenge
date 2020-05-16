b8e2edc2798f21998489650d97be856282746c51
PriyangaPKini <priyangapkini99@gmail.com>
2020-05-16 23:17:32 +0530

    Specified start up command for api and used env variables for databse connection

--- api/database.sql
+++ api/database.sql
@@ -1,25 +1,25 @@
 CREATE DATABASE customerservicedb;
 
-CREATE TABLE customer(
-    customer_id SERIAL PRIMARY KEY,
-    name VARCHAR(20),
-    age VARCHAR(5),
-    gender VARCHAR(10),
-    email VARCHAR(40),
-    phone VARCHAR(20),
-    address_id int DEFAULT NULL,
-    CONSTRAINT address
-    FOREIGN KEY (address_id) 
-    REFERENCES address(id) 
-    ON DELETE SET DEFAULT
-);
+-- CREATE TABLE customer(
+--     customer_id SERIAL PRIMARY KEY,
+--     name VARCHAR(20),
+--     age VARCHAR(5),
+--     gender VARCHAR(10),
+--     email VARCHAR(40),
+--     phone VARCHAR(20),
+--     address_id int DEFAULT NULL,
+--     CONSTRAINT address
+--     FOREIGN KEY (address_id) 
+--     REFERENCES address(id) 
+--     ON DELETE SET DEFAULT
+-- );
 
-CREATE TABLE address(
-    id SERIAL PRIMARY KEY, 
-    city VARCHAR(20),
-    state VARCHAR(20) ,
-    zipcode VARCHAR(10)    
-    );
+-- CREATE TABLE address(
+--     id SERIAL PRIMARY KEY, 
+--     city VARCHAR(20),
+--     state VARCHAR(20) ,
+--     zipcode VARCHAR(10)    
+--     );
 
 CREATE TABLE customers(
     customer_id SERIAL PRIMARY KEY,
